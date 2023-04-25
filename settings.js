2ac46e39dff40d0368b1c1c0777b9456cb5e55b3
rajapratama <131827794+rajapratama@users.noreply.github.com>
2023-04-26 07:23:33 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -46,18 +46,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Raja Store Bot" //namabot kalian
+global.ownername = "Raja Store" //nama kalian
+global.myweb = "." //bebas asal jan hapus
+global.youtube = "." //bebas asal jan hapus
+global.myweb2 = "." //bebas
+global.email = "kyzenkyzen555@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.premium = ['6283159647567'] //premium user
+global.owner = ["6283159647567", "6283869126523"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283159647567" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
